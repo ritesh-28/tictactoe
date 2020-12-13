04f3d4069df2c8c6eaa675d0dd7c590d7b49f7da
ritesh-28 <66766694+ritesh-28@users.noreply.github.com>
2020-12-13 19:52:28 +0530

    Add files via upload

---
+++ java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    // State Meanings
+    // 0 = X || 1 = O || 2 = Null
+    int activePlayer = 0;
+    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
+    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}};
+    public void playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(!gameActive){
+            gameReset();
+        }
+        if(gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn - Tap to play");
+            } else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        // Check if any player has won
+        for(int[] winPosition: winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]]!=2){
+                // Somebody has won! - Find out who!
+                String winnerStr;
+                gameActive = false;
+                if(gameState[winPosition[0]] == 0){
+                    showDialog("X is winner");
+                }
+                else{
+                    showDialog("O is winner");
+                }
+
+            }
+        }
+
+        // Check if Match is Drawn
+        int count=0;
+        for(int i=0;i<9;i++) {
+            if(gameState[i] != 2) {
+                count++;
+            }
+        }
+        if(count==9) {
+            showDialog(" Match Draw");
+        }
+
+    }
+
+    public void showDialog(String winner) {
+        new AlertDialog.Builder(this)
+                .setTitle(winner)
+                .setPositiveButton("Restart Game", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        gameReset();
+                    }
+                })
+                .show();
+    }
+
+
+    public void gameReset() {
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++){
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
